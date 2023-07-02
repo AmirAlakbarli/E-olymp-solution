362f684b96eb6c5e2b4ca77cd6d9e6ca290ce772
AmirAlakbarli <emirelekberli@gmail.com>
2023-07-03 03:30:38 +0400

    Problem 877 solution added

---
+++ problem877.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int calculateMinScore(const vector<int> &cards)
+{
+    int n = cards.size();
+
+    // Create a 2D table to store the minimum scores
+    vector<vector<int>> minScores(n, vector<int>(n, 0));
+
+    // Fill the table diagonally from bottom-left to top-right
+    for (int gap = 2; gap < n; gap++)
+    {
+        for (int left = 0; left < n - gap; left++)
+        {
+            int right = left + gap;
+            minScores[left][right] = INT_MAX;
+
+            // Try removing each card between left and right
+            for (int k = left + 1; k < right; k++)
+            {
+                int score = cards[left] * cards[k] * cards[right];
+                minScores[left][right] = min(minScores[left][right], score + minScores[left][k] + minScores[k][right]);
+            }
+        }
+    }
+
+    return minScores[0][n - 1];
+}
+
+int main()
+{
+
+    int n;
+    scanf("%d", &n);
+    vector<int> cards(n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &cards[i]);
+
+    int minScore = calculateMinScore(cards);
+
+    printf("%d\n", minScore);
+
+    return 0;
+}
